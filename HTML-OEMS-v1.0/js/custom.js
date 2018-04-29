13e64db279aeaf88c6d4ab1fb8c690a3855ff7d5
Ganesh Lomte <Ganesh Lomte>
2018-04-30 00:20:35 +0530

    oems first commit

---
+++ HTML-OEMS-v1.0/js/custom.js
@@ -0,0 +1,47 @@
+// custom JS code 
+$(document).ready( function(){
+	// startDate and endDate deskBooking
+	$('#startDate-deskBooking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+	$('#endDate-deskBooking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+	
+
+	// startDate and endDate employee
+	$('#startDate-employee').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+	$('#endDate-employee').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+
+
+	// startDate and endDate carBooking
+	$('#startDate-carBooking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+	$('#endDate-carBooking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+
+	// startDate and endDate equipmentTracking
+	$('#startDate-equipmentTracking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+	$('#endDate-equipmentTracking').datepicker({
+		autoclose: true,
+		format: "dd/mm/yyyy"
+	});
+
+
+
+});
